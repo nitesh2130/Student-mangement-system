c8002933921ef2ad4fb049e8451980de12fc4238
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-01-13 20:56:48 +0530

    to make some utils files for backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,8 @@
 import express from 'express';
-//import {User} from './models/userModel.js';
+
 
 const app = express();
-// app.use(express.json());
-// // Route to create a user
-// app.post('/create-user', async (req, res) => {
-//   try {
-//     const { name, email, password, role } = req.body;
-//     const newUser = await User.create({ name, email, password, role });
-//     res.status(201).json(newUser);
-//   } catch (error) {
-//     console.error('Error creating user:', error);
-//     res.status(500).json({ message: 'Error creating user' });
-//   }
-// });
+
 
 
 export default app;
